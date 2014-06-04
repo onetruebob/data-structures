69967db95c7f7a7768608963461ccadef3f6c5f9
onetruebob <bob@bobowen.org>
2014-06-04 16:11:52 -0700

    Completed queue using psuedoclasical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.storageSize = 0;
+  this.headIdx = 0;
+  this.tailIdx = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.storageSize;
+};
 
+Queue.prototype.enqueue = function(val){
+  this.storageSize++;
+  this.storage[this.tailIdx] = val;
+  this.tailIdx++;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.storageSize > 0) {
+    this.storageSize--;
+    var val = this.storage[this.headIdx];
+    delete this.storage[this.headIdx];
+    this.headIdx++;
+    return val;
+  }
+  return undefined;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storageSize = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.storageSize;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+
+Stack.prototype.pop = function() {
+  if(this.storageSize > 0) {
+    this.storageSize--;
+    var val = this.storage[this.storageSize];
+    delete this.storage[this.storageSize];
+    return val;
+  }
+  return undefined;
+};
 
