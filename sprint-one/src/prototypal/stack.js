8377ef47cf954a23644542735d87bd301ddb89ed
onetruebob <bob@bobowen.org>
2014-06-04 20:36:03 -0700

    Timing for performance testing of stack using prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,31 +1,44 @@
-var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  var newStack = Object.create(stackMethods);
-  newStack.storageSize = 0;
-  newStack.storage = {};
-  return newStack;
-};
+(function(){
+ makeStack = function() {
+    // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+    // but try not not reference your old code in writing the new style.
+    var newStack = Object.create(stackMethods);
+    newStack.storageSize = 0;
+    newStack.storage = {};
+    return newStack;
+  };
 
-var stackMethods = {};
+  var stackMethods = {};
 
-stackMethods.size = function() {
-  return this.storageSize;
-};
+  stackMethods.size = function() {
+    return this.storageSize;
+  };
 
-stackMethods.push = function(value){
-  this.storage[this.storageSize] = value;
-  this.storageSize++;
-};
+  stackMethods.push = function(value){
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  };
 
-stackMethods.pop = function(){
-  if(this.storageSize > 0) {
-    this.storageSize--;
-    var val = this.storage[this.storageSize];
-    delete this.storage[this.storageSize];
+  stackMethods.pop = function(){
+    if(this.storageSize > 0) {
+      this.storageSize--;
+      var val = this.storage[this.storageSize];
+      delete this.storage[this.storageSize];
 
-    return val;
+      return val;
+    }
+
+    return undefined;
+  };
+
+  var start = Date.now();
+
+
+  var stack = makeStack();
+  for (var i = 0; i<1000000; i++) {
+    stack.push(i);
+    stack.pop();
   }
 
-  return undefined;
-};
+  console.log('prototypal: ' + (Date.now() - start));
+})();
