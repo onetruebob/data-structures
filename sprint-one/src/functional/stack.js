62f40b9d48bee4ccb08472248d7a8d58ce7698ea
onetruebob <bob@bobowen.org>
2014-06-04 20:28:09 -0700

    execution time logging for functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,28 +1,42 @@
-var makeStack = function(){
-  var someInstance = {};
+(function(){
+  var makeStack = function(){
+    var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
-  var size = 0;
-  // Implement the methods below
-  someInstance.push = function(value){
-    storage[size] = value;
-    size++;
+    // Use an object with numeric keys to store values
+    var storage = {};
+    var size = 0;
+    // Implement the methods below
+    someInstance.push = function(value){
+      storage[size] = value;
+      size++;
+    };
+
+    someInstance.pop = function(){
+      if(size > 0) {
+        size--;
+        var val = storage[size];
+        delete storage[size];
+        return val;
+      }
+      return undefined;
+    };
+
+    someInstance.size = function(){
+      return size;
+    };
+
+    return someInstance;
   };
 
-  someInstance.pop = function(){
-    if(size > 0) {
-      size--;
-      var val = storage[size];
-      delete storage[size];
-      return val;
-    }
-    return undefined;
-  };
 
-  someInstance.size = function(){
-    return size;
-  };
+  var start = Date.now();
 
-  return someInstance;
-};
+
+  var stack = makeStack();
+  for (var i = 0; i<1000000; i++) {
+    stack.push(i);
+    stack.pop();
+  }
+
+  console.log('functional: ' + (Date.now() - start));
+})();
