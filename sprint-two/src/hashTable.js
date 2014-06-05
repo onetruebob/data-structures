ba4cda9ee96852fc4b322bdaa97861831bbbdf26
onetruebob <bob@bobowen.org>
2014-06-05 11:36:20 -0700

    Finished basic requirements for hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,18 +5,53 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var valueList;
+
+  if(!this._storage.get(i)) {
+    this._storage.set(i, {});
+  }
+  valueList = this._storage.get(i);
+
+  if(!valueList.hasOwnProperty(k)) {
+    valueList[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var valueList = this._storage.get(i);
+  if(valueList.hasOwnProperty(k)) {
+    return valueList[k];
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var valueList = this._storage.get(i);
+
+  valueList[k] = null;
+};
+
+HashTable.prototype.hashSize = function(){
+  //get the size
+  var keyTally;
+  this._storage.each(function(e){
+    if(e){
+      keyTally++;
+    }
+  });
+
+  return keyTally;
+};
+
+HashTable.prototype.checkExpansionNeeded = function(){
 
 };
 
+HashTable.prototype.checkContractionNeeded = function(){
 
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
