ed2b7d65b71a217cbe07cc0bef7c8901eedddd57
onetruebob <bob@bobowen.org>
2014-06-06 15:46:05 -0700

    finished addnode, contains, addedge, getedge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,6 +2,7 @@ var Graph = function(){
   // Node format _nodes =
   // {'puppies': {edges: {kittens: true}}, 'kittens': {edges: {puppies: true}}}
   this._nodes = {};
+  this._nodeCount = 0;
 
 };
 
@@ -10,24 +11,40 @@ var Node = function(){
 };
 
 Graph.prototype.addNode = function(newNodeId, toNodeId){
+  if (this._nodeCount === 1) {
+    toNodeId = Object.keys(this._nodes)[0];
+  }
   this._nodes[newNodeId] = new Node();
-
+  if (this._nodeCount > 0) {
+    this.addEdge(newNodeId, toNodeId);
+  }
+  this._nodeCount++;
 };
 
-Graph.prototype.contains = function(node){
-  return this._nodes.hasOwnProperty(node);
+Graph.prototype.contains = function(nodeId){
+  return this._nodes.hasOwnProperty(nodeId);
 };
 
-Graph.prototype.removeNode = function(node){
+Graph.prototype.removeNode = function(nodeId){
+  var temp = this._nodes[nodeId];
+
+  delete this._nodes[nodeId];
+  this._nodeCount--;
 };
 
-Graph.prototype.getEdge = function(fromNode, toNode){
+Graph.prototype.getEdge = function(fromNodeId, toNodeId){
+  return this._nodes[fromNodeId].edges.hasOwnProperty(toNodeId);
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
+Graph.prototype.addEdge = function(fromNodeId, toNodeId){
+  var fromNode = this._nodes[fromNodeId];
+  var toNode = this._nodes[toNodeId];
+
+  fromNode.edges[toNodeId] = true;
+  toNode.edges[fromNodeId] = true;
 };
 
-Graph.prototype.removeEdge = function(fromNode, toNode){
+Graph.prototype.removeEdge = function(fromNodeId, toNodeId){
 };
 
 /*
